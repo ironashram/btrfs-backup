694c2c4897bf41b584814bf1e4397d6014a88e11
ironashram <sysdadmin@m1k.cloud>
2022-01-21 01:04:17 +0100

    add laptop script

---
+++ snap/btrfs-snap_laptop.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#Create snapshotshots of root and home
+btrfs subvolume snapshot -r / /root-snapshot.$(date +%d-%m-%Y)
+sync
+
+# Delete snapshotshots older than 7 days
+find /* -maxdepth 0 -mtime +7 -name "root-snapshot.*" | xargs btrfs subvolume delete 
